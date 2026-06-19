69afaca8fe04633f5784154576ab7259bbecd279
Fabio Alves <akaselvas@gmail.com>
2026-06-19 19:29:22 -0300

    add testIdAttribute: 'data-test' to get tests with getByTestId()

--- tests/product-overview.spec.ts
+++ tests/product-overview.spec.ts
@@ -1,14 +1,17 @@
 import {test, expect, Locator} from "@playwright/test"
 
-test('Product overview is displayed @sprint1 @AC1', async ({ page }) => {
+
+//------ SPRINT 1 --------------------------------------------------//
+
+test('Product overview is displayed @AC1', async ({ page }) => {
     // Given I navigate to the home page
     // Then a grid of product cards is displayed showing all products. 
     
     await page.goto('https://with-bugs.practicesoftwaretesting.com/#/');
     await expect(page.locator('.card')).toHaveCount(9);
-});
+})
 
-test('Product card information @sprint1 @AC2', async ({page}) =>{
+test('Product card information @AC2', async ({page}) =>{
     // Given the product overview is displayed
     // Then each product card shows: a product imagem, the product name, the product price
     
@@ -24,7 +27,7 @@ test('Product card information @sprint1 @AC2', async ({page}) =>{
     }
 })
 
-test('Navigating to product detail @sprint1 @AC3', async ({page}) => {
+test('Navigating to product detail @AC3', async ({page}) => {
     // Given the product overview is displayed
     // When I click on a product card
     // Then I am navigated to the product detail page for that product. 
@@ -40,3 +43,55 @@ test('Navigating to product detail @sprint1 @AC3', async ({page}) => {
     await expect(page).toHaveURL(new RegExp(targetUrl!));
 })
 
+test('Search @AC4', async({page}) =>{
+    // Given I enter a valid search query (3–40 characters) and submit
+    // Then the product grid updates to show only matching products
+    // And all active filters are reset. 
+    await page.goto('https://with-bugs.practicesoftwaretesting.com');
+    
+    const searchQuery = page.locator('[data-test="search-query"]');
+    await expect(searchQuery).toBeVisible();
+    await searchQuery.fill('Hammer');
+
+    const searchButton = page.locator('[data-test="search-submit"]');
+    await searchButton.click();
+
+    await expect(page.locator('[data-test="search-term"]')).toBeVisible();
+    await expect(page.locator('[data-test="search-term"]')).toContainText('Hammer', {ignoreCase: true});
+
+    const notFilters = await page.locator('[type="checkbox"]').all();
+    
+    for (const checkboxes of notFilters){
+        await expect(checkboxes).not.toBeChecked();
+    }
+
+})
+
+
+test('Category filter @AC5', async({page}) =>{
+    // Given I check one or more category checkboxes in the sidebar
+    // Then the product grid updates to show only products from those categories. 
+    await page.goto('https://with-bugs.practicesoftwaretesting.com');
+
+    await expect(page.locator('div[data-test="filters"]')).toBeVisible();
+
+    const checkHammer = page.locator('[data-test="category-3"]');
+    const constHandSaw = page.locator('[data-test="category-4"]');
+    await checkHammer.check();
+    await constHandSaw.check();
+
+    await expect(page.locator('[data-test="filter_completed"]')).toBeVisible();
+    
+    // 1. Declaramos a expressão de busca uma única vez
+    const allowedCategories = /Hammer|Saw/i;
+
+    // 2. Usamos fora do loop para forçar a espera do primeiro produto
+    await expect(page.locator('[data-test="product-name"]').first()).toContainText(allowedCategories);
+
+    // 3. Usamos dentro do loop para validar todos de forma curta
+    const eachCard = await page.locator('.card').all();
+    for (const card of eachCard) {
+        await expect(card.locator('[data-test="product-name"]')).toContainText(allowedCategories);
+    }
+        
+})
