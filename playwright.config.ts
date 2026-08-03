19a195380d4bc7fd2644ce1ca14d767f1ab5fbd6
Fabio Alves <akaselvas@gmail.com>
2026-08-03 20:24:17 -0300

    ci docker image

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    baseURL: process.env.URL || 'https://practicesoftwaretesting.com',
+    apiURL: process.env.API_URL || 'https://api.practicesoftwaretesting.com',
+
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
